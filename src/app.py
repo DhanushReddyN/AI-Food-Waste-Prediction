2df34d0462f6dbba9dd28b8d6cf3dd3856387f77
DhanushReddyn <dhanushreddyn831@gmail.com>
2026-05-09 09:50:44 +0530

    fixed railway port

--- src/app.py
+++ src/app.py
@@ -88,7 +88,7 @@ def predict():
 
 
 # Run Flask App
+import os
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
